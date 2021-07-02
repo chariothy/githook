97c95465c3ccd2a625dfb54fa38910bf6c59bfc1
henry <henrytian@vip.qq.com>
2021-07-02 18:16:19 +0800

    Default update.

--- start.sh
+++ start.sh
@@ -44,7 +44,7 @@ fi
 
 if [ "$ENV" == "prod" ]; then
         docker-compose down
-        docker-compose up -d \
+        docker-compose up -d --build \
     &&  docker-compose logs -f --tail=10
 else
         docker-compose build \
